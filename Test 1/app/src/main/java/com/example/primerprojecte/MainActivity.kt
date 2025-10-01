13abb0531e7a0fe15858d86b6af0005f25b4ec6f
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-10-01 16:24:49 +0200

    Fins exercici2

--- Test 1/app/src/main/java/com/example/primerprojecte/MainActivity.kt
+++ Test 1/app/src/main/java/com/example/primerprojecte/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.primerprojecte
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +14,34 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         println("Hello, world!")
-        }
+       //1
+        val MultiplicarNumeros = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        for (num in MultiplicarNumeros) {
+            val resultat=num*5
+
+            println("multi: $resultat")
+
+         //2- Feu una funció que ens mostri Fred, Tebi o Calent segons una temperatura passada per paràmetre.
+         fun Sensaciotermica(temp: Int): String {
+             if (temp<10)
+             {
+              return "Fred"
+             }
+             else if (temp>10&&temp<25)
+                 return "Tebi"
+             else
+                 return "Calent"
+
+         }
+
+            fun main() {
+                println("A 3 Graus és: ${Sensaciotermica(temp=3)}")
+                println("A 20 Graus és: ${Sensaciotermica(temp=20)}")
+                println("A 30 Graus és: ${Sensaciotermica(temp=30)}")
+            }
+
+
     }
+}}
+
+
