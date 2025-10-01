54a5edccf84786648dbae22fda040cc6619e0dbd
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-10-01 15:40:11 +0200

    canvi al hello worl55d

--- Test 1/app/src/main/java/com/example/primerprojecte/MainActivity.kt
+++ Test 1/app/src/main/java/com/example/primerprojecte/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//1- Mostreu per consola  La taula de multiplicar del 5
+//
+//2- Feu una funció que ens mostri Fred, Tebi o Calent segons una temperatura passada per paràmetre.
+//
+//3- Feu una funció que retorni l'arrel quadrada d'un número passat per paràmetre.
 
         setContentView(R.layout.activity_main)
         println("Hello, world!")
