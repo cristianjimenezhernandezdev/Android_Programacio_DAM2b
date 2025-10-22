d1225a27c75693cc32815cee18d91637d8418870
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-10-22 15:44:16 +0200

    app cicles1

---
+++ CicledeVida/app/src/main/java/com/example/cicledevida/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cicledevida
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        println("OnCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("OnStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("OnRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("OnStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("OnDestroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("OnPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("OnResume")
+
+    }
+}
