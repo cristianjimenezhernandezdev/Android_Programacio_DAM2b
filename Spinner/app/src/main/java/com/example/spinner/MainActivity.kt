ebd0aa72d8044ec431ec58898125035910ba3b5f
cristianjimenezhernandez <cristian.jimenez.dev@gmail.com>
2025-11-05 20:00:25 +0100

    canvis cap a afegir segon spinner

--- Spinner/app/src/main/java/com/example/spinner/MainActivity.kt
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.kt
@@ -17,11 +17,11 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 lateinit var adapter:ArrayAdapter<CharSequence>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val interessos = listOf("Plataformes", "Novela Grafica", "RPG", "Estratègia", "JRPG", "Carreres")
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val spinner: Spinner = binding.planetsSpinner
+        ////Per els Spinner Planetes----------------------------
+
+        val spinnerPlanetes: Spinner = binding.planetsSpinner
 // Create an ArrayAdapter using the string array and a default spinner layout.
         adapter = ArrayAdapter.createFromResource(
             this,
@@ -31,8 +31,21 @@ lateinit var adapter:ArrayAdapter<CharSequence>
             // Specify the layout to use when the list of choices appears.
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner.
-            spinner.adapter = adapter
-            spinner.onItemSelectedListener = this
+            spinnerPlanetes.adapter = adapter
+            spinnerPlanetes.onItemSelectedListener = this
+//////////////Per el segon spinner fent servis listof, bàsicament copio el codi i modifico una mica
+        val spinnerInteressos: Spinner = binding.interessosSpinner
+        val interessos = listOf("Plataformes", "Novela Grafica", "RPG", "Estratègia", "JRPG", "Carreres")
+
+        // Creem un NOU adaptador per aquest Spinner
+        val adapterInteressos = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            interessos // Passem la llista de Kotlin
+        )
+        adapterInteressos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerInteressos.adapter = adapterInteressos
+        spinnerInteressos.onItemSelectedListener = this
 
         }
 
