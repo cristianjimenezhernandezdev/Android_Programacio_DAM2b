0d3cc2384de4d3c4442d2851f40897908f7af84b
cristianjimenezhernandezdev <cristian.jimenez.dev@gmail.com>
2025-12-03 16:35:32 +0100

    Seguint instruccions, creo el .kt OddEventEdit com diu

---
+++ CustomView_Cristian/app/src/main/java/com/example/customviewcristian/OddEvenEditText.kt
@@ -0,0 +1,12 @@
+package com.example.customviewcristian
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class OddEvenEditText : AppCompatEditText {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+}
+
